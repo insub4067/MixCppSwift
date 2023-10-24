1a6106fc2263a791614b669ca72c4612ada51c32
insub <vb15@kakao.com>
2023-10-24 10:36:33 +0900

    Update ContentView.swift

--- CppPractice/ContentView.swift
+++ CppPractice/ContentView.swift
@@ -18,6 +18,7 @@ struct ContentView: View {
         }
         .padding()
         .onAppear(perform: {
+            // available from macOS 13.3.0
             if #available(iOS 16.4.0, *) {
                 let mountain = Mountain.create("관악산", 632)
                 mountain.displayInfo()
